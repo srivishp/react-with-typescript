80a21e3f7b9499ef2a478e40d14e4e3c59264205
Sri Vishnu <srivishp@gmail.com>
2023-12-01 12:41:12 +0530

    Create Todos component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import "./App.css";
+import Todos from "./components/Todos";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Todos />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Todos.tsx
@@ -0,0 +1,10 @@
+function Todos() {
+  return (
+    <ul>
+      <li>Item 1</li>
+      <li>Item 2</li>
+    </ul>
+  );
+}
+
+export default Todos;
